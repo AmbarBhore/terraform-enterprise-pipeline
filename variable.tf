1715c4924ab1a9585afc1186f91d23a7ae428ef1
AmbarBhore1234 <ambarbhore1234@gmail.com>
2025-08-11 15:14:40 +0530

    removed everything

--- variable.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_region" {
-  description = "AWS region to deploy resources in"
-  type = string
-}
-
-variable "bucket_name" {
-  description = "name of the bucket"
-  type = string
-}

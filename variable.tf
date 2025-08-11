b55d8e33365b7d57fc375b259dab82114c500a18
AmbarBhore1234 <ambarbhore1234@gmail.com>
2025-08-11 13:06:28 +0530

    modified terraform files

--- variable.tf
+++ variable.tf
@@ -3,18 +3,6 @@ variable "aws_region" {
   type = string
 }
 
-variable "aws_access_key" {
-  description = "AWS access key"
-  type = string
-  sensitive = true
-}
-
-variable "aws_secret_key" {
-  description = "AWS secret key"
-  type = string
-  sensitive = true
-}
-
 variable "bucket_name" {
   description = "name of the bucket"
   type = string

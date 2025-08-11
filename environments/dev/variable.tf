f681f4b8fb89acec540daacc0dc043bf40c28619
AmbarBhore1234 <ambarbhore1234@gmail.com>
2025-08-11 19:07:30 +0530

    added s3 modules

--- environments/dev/variable.tf
+++ environments/dev/variable.tf
@@ -2,3 +2,8 @@ variable "user_name" {
   description = "IAM user name for the dev environment"
   type        = string
 }
+
+variable "bucket_name" {
+  description = "This is bucket name variable"
+  type = string
+}

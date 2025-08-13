dfa95dc39901efd96de575570cc9e9a1525f2a00
AmbarBhore1234 <ambarbhore1234@gmail.com>
2025-08-13 23:47:02 +0530

    adding envrionment

---
+++ environments/prod/output.tf
@@ -0,0 +1,11 @@
+output "dev_s3_bucket_name" {
+   value = module.bucket_name.bucket_name
+}
+
+output "dev_iam_user_name" {
+   value = module.dev_iam_user.user_name
+}
+
+output "dev_vpc_project" {
+   value = module.vpc_project.project_vpc
+}

051c4d41f4577a005f3ea36278ce9c671bcda750
AmbarBhore1234 <ambarbhore1234@gmail.com>
2025-08-13 14:04:33 +0530

    added drft detection 4 tags

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -2,6 +2,10 @@ resource "aws_s3_bucket" "example" {
    bucket = var.bucket_name
 
    tags = {
+      bucket = "logs"
+      enviornment = "dev"
+      location = "mumbai"
+      team = "finance"
       owner = "DevOps"
       role = "Director"
    }

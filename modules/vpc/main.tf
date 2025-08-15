94664e3d29d9d4905f571aba7ad7a9506361d9e6
AmbarBhore1234 <ambarbhore1234@gmail.com>
2025-08-15 14:38:26 +0530

    created new public route table and associated to the public subnet

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -20,3 +20,19 @@ resource "aws_subnet" "private" {
    cidr_block = var.private_subnet_cidr_block
    tags = merge(var.tags, { Name = "dev-private-subnet" })
 }
+
+resource "aws_route_table" "public_rt" {
+   vpc_id = aws_vpc.dev_vpc.id
+   
+   route {
+       cidr_block = "0.0.0.0/0"
+       gateway_id = aws_internet_gateway.gw.id
+   }
+   
+   tags = merge(var.tags, { Name = "Public_Route_Table" })
+}
+
+resource "aws_route_table_association" "public-rt-association" {
+   subnet_id = aws_subnet.public.id
+   route_table_id = aws_route_table.public_rt.id
+}

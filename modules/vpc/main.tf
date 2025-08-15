8ff220bcc67d8a69c43a6fe479b15cb9a724f49d
AmbarBhore1234 <ambarbhore1234@gmail.com>
2025-08-15 13:28:39 +0530

    adding Name to all VPC resources

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,21 +1,21 @@
 resource "aws_vpc" "dev_vpc" {
    cidr_block = var.cidr_block
-   tags       = var.tags
+   tags       = merge(var.tags, { Name = "dev-vpc" })
 }
 
 resource "aws_internet_gateway" "gw" {
    vpc_id = aws_vpc.dev_vpc.id
-   tags = var.tags
+   tags = merge(var.tags, { Name = "dev-IGW" })
 }
 
 resource "aws_subnet" "public" {
    vpc_id = aws_vpc.dev_vpc.id
    cidr_block = var.public_subnet_cidr_block
-   tags = var.tags
+   tags = merge(var.tags, { Name = "dev-public-subnet" })
 }
 
 resource "aws_subnet" "private" {
    vpc_id = aws_vpc.dev_vpc.id
    cidr_block = var.private_subnet_cidr_block
-   tags = var.tags
+   tags = merge(var.tags, { Name = "dev-private-subnet" })
 }

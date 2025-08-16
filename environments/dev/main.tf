105089657781d89e2fb0a13558b04b8c2ba2197c
AmbarBhore1234 <ambarbhore1234@gmail.com>
2025-08-16 22:24:02 +0530

    added # infront of all resouces that is nothing but destroying the resource

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,15 +1,14 @@
-#terraform {
-#   required_providers {
-#     aws = {
-#	source = "hashicorp/aws"
-#	version = "~> 5.0"
-#     }
-#   }
-#}
-#
-#provider "aws" {
-#   region = "ap-south-1"
-#}
+terraform {
+   required_providers {
+     aws = {
+	source = "hashicorp/aws"
+	version = "~> 5.0"
+     }
+   }
+}
+provider "aws" {
+   region = "ap-south-1"
+}
 #
 #module "dev_iam_user" {
 #   source = "../../modules/iam"

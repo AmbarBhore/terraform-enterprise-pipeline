34439418948f3b53f415c63895c8b841978f47af
AmbarBhore1234 <ambarbhore1234@gmail.com>
2025-08-16 22:18:19 +0530

    added # infront of all resouces that is nothing but destroying the resource

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,31 +1,31 @@
-terraform {
-   required_providers {
-     aws = {
-	source = "hashicorp/aws"
-	version = "~> 5.0"
-     }
-   }
-}
-
-provider "aws" {
-   region = "ap-south-1"
-}
-
-module "dev_iam_user" {
-   source = "../../modules/iam"
-   user_name = var.user_name
-}
-
-module "bucket_name" {
-   source = "../../modules/s3"
-   bucket_name = var.bucket_name
-}
-
-module "vpc_project" {
-   source = "../../modules/vpc"
-   cidr_block = var.cidr_block
-   public_subnet_cidr_block = var.public_subnet_cidr_block
-   private_subnet_cidr_block = var.private_subnet_cidr_block
-   availability_az = var.availability_az
-   tags = local.common_tags
-}
+#terraform {
+#   required_providers {
+#     aws = {
+#	source = "hashicorp/aws"
+#	version = "~> 5.0"
+#     }
+#   }
+#}
+#
+#provider "aws" {
+#   region = "ap-south-1"
+#}
+#
+#module "dev_iam_user" {
+#   source = "../../modules/iam"
+#   user_name = var.user_name
+#}
+#
+#module "bucket_name" {
+#   source = "../../modules/s3"
+#   bucket_name = var.bucket_name
+#}
+#
+#module "vpc_project" {
+#   source = "../../modules/vpc"
+#   cidr_block = var.cidr_block
+#   public_subnet_cidr_block = var.public_subnet_cidr_block
+#   private_subnet_cidr_block = var.private_subnet_cidr_block
+#   availability_az = var.availability_az
+#   tags = local.common_tags
+#}

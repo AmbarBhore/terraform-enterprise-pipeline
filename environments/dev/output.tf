cee8c13ab35d901395d14d74f1d8998cb0ed95ae
AmbarBhore1234 <ambarbhore1234@gmail.com>
2025-08-16 22:21:38 +0530

    added # infront of all resouces that is nothing but destroying the resource

--- environments/dev/output.tf
+++ environments/dev/output.tf
@@ -1,35 +1,35 @@
-output "dev_s3_bucket_name" {
-   value = module.bucket_name.bucket_name
-}
-
-output "dev_iam_user_name" {
-   value = module.dev_iam_user.user_name
-}
-
-output "dev_vpc" {
-   value = module.vpc_project.dev_vpc
-}
-
-output "dev_subnet" {
-   value = module.vpc_project.public_subnet
-}
-
-output "dev_private_subnet" {
-   value = module.vpc_project.private_subnet
-}
-
-output "public_route_table" {
-   value = module.vpc_project.public_rt
-}
-
-output "public_ip" {
-   value = module.vpc_project.public_ip
-}
-
-output "private_route_table" {
-   value = module.vpc_project.private_rt
-}
-
-output "nat_gateway" {
-   value = module.vpc_project.nat_gateway
-}
+#output "dev_s3_bucket_name" {
+#   value = module.bucket_name.bucket_name
+#}
+#
+#output "dev_iam_user_name" {
+#   value = module.dev_iam_user.user_name
+#}
+#
+#output "dev_vpc" {
+#   value = module.vpc_project.dev_vpc
+#}
+#
+#output "dev_subnet" {
+#   value = module.vpc_project.public_subnet
+#}
+#
+#output "dev_private_subnet" {
+#   value = module.vpc_project.private_subnet
+#}
+#
+#output "public_route_table" {
+#   value = module.vpc_project.public_rt
+#}
+#
+#output "public_ip" {
+#   value = module.vpc_project.public_ip
+#}
+#
+#output "private_route_table" {
+#   value = module.vpc_project.private_rt
+#}
+#
+#output "nat_gateway" {
+#   value = module.vpc_project.nat_gateway
+#}
